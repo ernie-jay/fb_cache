46a86ea9ab21013244baf86ef0d13378d1ef2604
Erno Jojart <ejojart@outlook.com>
2023-02-13 21:29:00 +0000

    Exception handling and managing backup of inner collections

--- FBCache/FbCache.cs
+++ FBCache/FbCache.cs
@@ -2,24 +2,171 @@
 {
     public class FbCache
     {
+        //Singleton instance
         private static FbCache? instance = null;
+        private static readonly object padlock = new object();
+
+        //using LinkedList to update operations
+        private LinkedList<(object, object)> cacheList;
+        private Dictionary<object, LinkedListNode<(object, object)>> cacheSet;
+        private int capacity = 1000;
+        public int Capacity {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                if (count < value)
+                {
+                    cacheSet.EnsureCapacity(value);
+                    
+                }else if (count > value)
+                {
+                    var trimmedList = cacheList.Take(value).ToList();
+                    LinkedList<(object, object)> newCacheList = new LinkedList<(object, object)> (trimmedList);
+                    Dictionary<object, LinkedListNode<(object, object)>> newCacheSet = new Dictionary<object, LinkedListNode<(object, object)>>(value);
+                    for (var node = newCacheList.First; node != null; node = node.Next)
+                    {
+                        newCacheSet.Add(node.Value.Item1, node);
+                    }
+                    cacheList = newCacheList;
+                    cacheSet = newCacheSet;
+                    count = value;
+                }
+                capacity = value;
+            }
+        }
+        private int count;
 
         private FbCache()
         {
+            cacheList = new LinkedList<(object, object)>();
+            cacheSet = new Dictionary<object, LinkedListNode<(object,object)>>(capacity);
+            count = 0;
         }
 
-        public static FbCache GetInstance()
+        public static FbCache Instance
         {
-            if (instance == null)
+            get
             {
-                instance = new FbCache();
+                if (instance == null)
+                { 
+                    lock (padlock)
+                    {
+                        instance ??= new FbCache();
+                    }
+                }
+                return instance;
             }
-            return instance;
         }
 
-        public V? GetData<K, V>(K key) { throw new NotImplementedException("method is not implemented"); }
-        public bool SetData<K, V>(K key, V data) { throw new NotImplementedException("method is not implemented"); }
-        public V? RemoveData<K, V>(K key) { throw new NotImplementedException("method is not implemented"); }
-        public bool Dispose() { throw new NotImplementedException("method is not implemented"); }
+        public V? GetData<K, V>(K key)
+        {
+            if (key == null) throw new ArgumentNullException("key");
+            lock (this)
+            {
+                var backupList = new LinkedList<(object, object)>(cacheList);
+                //if no element in the cache, then throw exception
+                if (!cacheSet.ContainsKey(key)) throw new KeyNotFoundException();
+                try
+                {
+                    var node = cacheSet[key];
+                    cacheList.Remove(node);
+                    cacheList.AddFirst(node);
+                    return (V)node.Value.Item2;
+                }catch
+                {
+                    cacheList = backupList;
+                    throw;
+                }
+            }
+        }
+        public bool SetData<K, V>(K key, V data) 
+        {
+            if (key == null) throw new ArgumentNullException("key");
+            if (data == null) throw new ArgumentNullException("data");
+            lock (this)
+            {
+                var backupList = new LinkedList<(object, object)>(cacheList);
+                var backupSet = new Dictionary<object, LinkedListNode<(object, object)>>(cacheSet);
+                object itemRemoved = null;
+                try
+                {
+                    //if there is a cache element with the same key, then remove it
+                    if (cacheSet.ContainsKey(key))
+                    {
+                        cacheList.Remove(cacheSet[key]);
+                        cacheSet.Remove(key);
+                        count--;
+                    }
+
+                    //if the cache is at capacity, remove the last item
+                    if (count == capacity)
+                    {
+                        var lastNode = cacheList.Last;
+                        itemRemoved = lastNode.Value.Item1;
+                        cacheList.RemoveLast();
+                        cacheSet.Remove(lastNode.Value.Item1);
+                        count--;
+                    }
+
+                    var node = cacheList.AddFirst((key, data));
+                    cacheSet.Add(key, node);
+                    count++;
+
+                }catch
+                {
+                    cacheList = backupList;
+                    cacheSet = backupSet;
+                    throw;
+                }
+            }
+
+            return true;
+        }
+        public bool RemoveData<K, V>(K key) 
+        {
+            if (key == null) return false;
+            if (!cacheSet.ContainsKey(key)) return false;
+            lock(this)
+            {
+                var backupList = new LinkedList<(object, object)>(cacheList);
+                var backupSet = new Dictionary<object, LinkedListNode<(object, object)>>(cacheSet);
+                try
+                {
+                    cacheList.Remove(cacheSet[key]);
+                    cacheSet.Remove(key);
+                    count--;
+                    return true;
+                }
+                catch {
+                    cacheList = backupList;
+                    cacheSet = backupSet;
+                    throw; 
+                }
+            }
+        }
+        public bool Dispose() 
+        {
+            lock (this)
+            {
+                var backupList = new LinkedList<(object, object)>(cacheList);
+                var backupSet = new Dictionary<object, LinkedListNode<(object, object)>>(cacheSet);
+                try
+                {
+                    cacheList = new LinkedList<(object, object)>();
+                    cacheSet = new Dictionary<object, LinkedListNode<(object, object)>>(capacity);
+                    cacheSet.EnsureCapacity(capacity);
+                    count = 0;
+                }catch
+                {
+                    cacheList = backupList;
+                    cacheSet = backupSet;
+                    throw;
+                }
+                return true;
+            }
+        }
     }
 }
--- FBCacheTests/FBCacheTests.cs
+++ FBCacheTests/FBCacheTests.cs
@@ -8,8 +8,8 @@ namespace FBCacheTests
         [Fact]
         public void TestSingleton()
         {
-            var cache1 = FbCache.GetInstance();
-            var cache2 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
+            var cache2 = FbCache.Instance;
 
             Assert.Equal(cache1, cache2);
         }
@@ -17,7 +17,7 @@ namespace FBCacheTests
         [Fact]
         public void TestAddElement()
         {
-            var cache1 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
             cache1.Dispose();
             
             var success = cache1.SetData<int, string>(1, "one");
@@ -28,33 +28,36 @@ namespace FBCacheTests
         [Fact]
         public void TestGetElement()
         {
-            var cache1 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
             cache1.Dispose();
 
             cache1.SetData(1, "one");
-            var result = cache1.GetData<int, string>(1);
+            cache1.SetData("two", 2);
 
-            Assert.Equal("one", result);
+            var result1 = cache1.GetData<int, string>(1);
+            var result2 = cache1.GetData<string, int>("two");
+
+            Assert.Equal("one", result1);
+            Assert.Equal(2,result2);
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int, int>(2));
         }
 
         [Fact]
         public void TestRemoveElement()
         {
-            var cache1 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
             cache1.Dispose();
 
             cache1.SetData(1, "one");
             cache1.RemoveData<int, string>(1);
 
-            var result = cache1.GetData<int, string>(1);
-
-            Assert.Null(result);
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int, string>(1));
         }
 
         [Fact]
         public void TestDispose() 
         {
-            var cache1 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
             cache1.Dispose();
 
             cache1.SetData(1, "one");
@@ -63,30 +66,70 @@ namespace FBCacheTests
 
             cache1.Dispose(); 
 
-            var result1 = cache1.GetData<int, string>(1);
-            var result2 = cache1.GetData<int, string>(2);
-            var result3 = cache1.GetData<int, string>(3);
-
-            Assert.Null(result1);
-            Assert.Null(result2);
-            Assert.Null(result3);
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int, string>(1));
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int, string>(2));
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int, string>(3));
         }
 
         [Fact]
         public void TestThreadSafety()
         {
-            var cache1 = FbCache.GetInstance();
+            var cache1 = FbCache.Instance;
             cache1.Dispose();
 
-            int lastResult;
             var random = new Random();
 
-            Parallel.For(0, 100000, i =>
+            _ = Parallel.For(0, 100000, i =>
             {
                 int value = random.Next(0, 100000);
-                cache1.SetData(1, value);
+                var cache2 = FbCache.Instance;
+                cache2.SetData(i, value);
+                try
+                {
+                    var result = cache1.GetData<int, int>(i);
+                    Assert.Equal(value, result);
+                } catch(KeyNotFoundException)
+                {
+                    Assert.True(true);
+                }
             });
 
+        }
+
+        [Fact]
+        public void TestResizeCache()
+        {
+            var cache1 = FbCache.Instance;
+            cache1.Dispose();
+
+            cache1.Capacity = 10;
+            for(int i=0; i<10; i++)
+            {
+                cache1.SetData(i, i);
+            }
+            cache1.Capacity = 5;
+            int result = cache1.GetData<int, int>(6);
+            Assert.Equal(6, result);
+            Assert.Throws<KeyNotFoundException>(() => cache1.GetData<int,int>(1));
+        }
+
+        [Fact]
+        public void TestCapacity()
+        {
+            var cache1 = FbCache.Instance;
+            cache1.Capacity = 5;
+            cache1.Dispose();
+
+            for(int i=1; i<=5; i++)
+            {
+                cache1.SetData(i, i);
+            }
+
+            cache1.SetData(6, "Six");
+
+            Assert.Throws<KeyNotFoundException>(()=>cache1.GetData<int, int>(1));
+            Assert.Equal(2, cache1.GetData<int, int>(2));
+            Assert.Equal("Six", cache1.GetData<int, string>(6));
         }
 
     }
